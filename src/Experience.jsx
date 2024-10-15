c80b2b0672d9c0a79ba8f2690b271382f68f26f1
jawid-fakhari <jawid_fakhari@live.it>
2024-10-15 21:50:22 +0200

    Import Other parts of Model

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,19 +1,46 @@
-import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
+import { Center, OrbitControls, useGLTF, useTexture } from '@react-three/drei'
 
 export default function Experience() {
     const bakedTexture = useTexture('./model/baked.jpg')
     bakedTexture.flipY = false
+
+    //importare i nodi del modello
     //al posto andare a trovare model.nodes, distructure model e scrivi nodes
     const { nodes } = useGLTF('./model/portal.glb')
 
     return <>
+
         <color args={['#030202']} attach="background" />
         <OrbitControls makeDefault />
 
-        {/* nei modelli complessi non usiamo <Primitive /> ma creiamo mesh */}
-        <mesh geometry={nodes.baked.geometry}>
-            <meshBasicMaterial map={bakedTexture} />
-        </mesh>
+        <Center>
+            {/* nei modelli complessi non usiamo <Primitive /> ma creiamo mesh */}
+            <mesh geometry={nodes.baked.geometry}>
+                <meshBasicMaterial map={bakedTexture} />
+            </mesh>
+
+            {/* importare la geometry della luceA & B */}
+            <mesh
+                geometry={nodes.poleLightA.geometry}
+                position={nodes.poleLightA.position} //applicare la posizione giusta(come nel blender)
+            >
+                <meshBasicMaterial color="#ffffe5" />
+            </mesh>
+            <mesh
+                geometry={nodes.poleLightB.geometry}
+                position={nodes.poleLightB.position} //applicare la posizione giusta(come nel blender)
+            >
+                <meshBasicMaterial color="#ffffe5" />
+            </mesh>
+
+            {/* importare portalLight e applicare posizione e rotazione (come nel blender)*/}
+            <mesh
+                geometry={nodes.portalLight.geometry}
+                position={nodes.portalLight.position}
+                rotation={nodes.portalLight.rotation}
+            >
+            </mesh>
+        </Center>
 
     </>
 }
